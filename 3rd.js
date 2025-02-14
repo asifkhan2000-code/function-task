65c0340cda704ed3c7371fc89d1ec946720b71df
asifkhan2000-codee <asifononto2000@gmail.com>
2025-02-15 01:04:41 +0600

    3rd task is done

---
+++ 3rd.js
@@ -0,0 +1,16 @@
+function make_avg(arr, arrLen) {
+    console.log("The input array is:", arr);
+    let sum = 0;
+    console.log("Index values of the array are: ");
+    for (let i = 0; i < arrLen; i++) {
+        let num = arr[i];
+        console.log(num);
+        sum = sum + num;
+    }
+    avg = sum / arrLen;
+    return avg;
+}
+const nums = [30, 40, 50, 60, 70];
+const len = nums.length;
+const result = make_avg(nums, len);
+console.log("Average of the values of the array:", result);
